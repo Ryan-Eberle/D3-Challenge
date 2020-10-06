44d6eafb9955caeda66dbb20e942773b9d931f97
Ryan-Eberle <ryanreberle@gmail.com>
2020-10-06 16:05:32 -0600

    update functions

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -52,24 +52,84 @@ function renderXAxis(newXScale, Xaxis) {
         .call(bottomAxis);
     return xAxis;
 }
+//function for updating Yaxis var on click
+function renderYAxis(newYScale, Yaxis) {
+    var leftAxis = d3.axisLeft(newYScale);
 
-function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+    Yaxis.transition()
+        .duration(1000)
+        .call(leftAxis);
+    return Yaxis;
+}
+// function to render the data point circles using X and Y scales
+function renderCircles(circlesGroup, newXScale, newYScale, chosenXAxis, chosenYAxis) {
     circlesGroup.transition()
         .duration(1000)
-        .attr("cx", d => newXScale(d[chosenXAxis]));
-
+        .attr("cx", d => newXScale(d[chosenXAxis]))
+        .attr("cy", d => newYScale(d[chosenYAxis]));
     return circlesGroup;
 }
 
-function updateToolTip(chosenXAxis, circlesGroup) {
-    var label;
+// function to format text for renderCircles
+function renderText(circlesText, newXScale, newYScale, chosenXAxis, chosenYAxis) {
+    circlesText.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenXAxis]))
+        .attr("cy", d => newYScale(d[chosenYAxis]));
+    return circlesText;
+}
+// ToolTip function for X and Y 
+function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup, circlesText) {
+    var xLabel;
+    var xformatLabel;
 
     if (chosenXAxis === "obesity") {
-        label = "Obeseity % :";
+        label = "Obeseity: "
+        xformatLabel = "%";
     }
-    else{}
+    else if (chosenXAxis === "smokes"){
+        label = "Smokes: "
+        xformatLabel = "%";
+    }
+    else {
+        label = "Lacks Healthcare: "
+        xformatLabel = "%";
+    }
+
+    var yLabel;
+    var yformatLabel
+
+    if (chosenYAxis === "age") {
+        label = "Age: "
+        yformatLabel = " ";
+    }
+    else if (chosenYAxis === "poverty") {
+        label = "Poverty: "
+        yformatLabel = "%";
+    }
+    else {
+        label = "Household Income: $"
+        yformatLabel = " ";
+    }
+    // format tooltip variable show state data 
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .html(function(d) {
+            return (`${d.state}<br>
+            ${xLabel}${chosenXAxis}${xformatLabel}<br>
+            ${yLabel}${chosenYAxis}${yformatLabel}`)
+        });
+
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data);
+    })
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+    return circleGroup;
 }
-    
 
 
 d3.csv('/assets/data/data.csv').then(function(newsData) {
